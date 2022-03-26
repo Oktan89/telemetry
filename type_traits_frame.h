257c8c5eec0d849551b5a33f4606d470cb0ad828
-=Oktan=- <r.zhigalenko@mail.ru>
2022-03-27 00:53:21 +0500

    fix

--- type_traits_frame.h
+++ type_traits_frame.h
@@ -54,16 +54,16 @@ struct Quality
     uint8_t value;
     std::string getQuality()
     {
-        
-        std::bitset<3> q(value);
+        std::bitset<8> q(value);
         std::string quality;
-        if(q.none()) quality = "Invalid";
+        if(q.none()) quality = " Invalid ";
         if(q.any())
         {
-            if(q.test(0)) quality = "Valid";
-            if(q.test(1)) quality += "Substituted";
-            if(q.test(2)) quality = "Invalid, Overflow";
+            if(q.test(0)) quality = " Valid ";
+            if(q.test(1)) quality += " Substituted ";
+            if(q.test(2)) quality += " Overflow ";
         }
+                
         return quality;
     }
 };
